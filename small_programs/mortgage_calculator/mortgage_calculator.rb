ded8e01513c8ef604766ef44cb5e7df69fe73c25
Tom McPherson <tom.mcpherson@outlook.com>
2023-02-19 12:56:55 +0000

    Incorporating code review feedback for mortgage calculator

--- small_programs/mortgage_calculator/mortgage_calculator.rb
+++ small_programs/mortgage_calculator/mortgage_calculator.rb
@@ -20,11 +20,8 @@ def get_loan_amount
   loop do
     prompt("Please enter your loan amount in a number format (e.g. 100000):")
     loan_amount = gets.chomp
-    if valid_number?(loan_amount) == false
-      prompt("Invalid loan amount. Please provide a positive number")
-    else
-      break
-    end
+    break if valid_number?(loan_amount) == true
+    prompt("Invalid loan amount. Please provide a positive number")
   end
 
   loan_amount
@@ -37,11 +34,8 @@ def get_apr
   loop do
     prompt("Please enter your APR in a percentage format (e.g. 5%):")
     apr = gets.chomp
-    if valid_percentage?(apr) == false
-      prompt("Invalid APR. Please provide a percentage")
-    else
-      break
-    end
+    break if valid_percentage?(apr) == true
+    prompt("Invalid APR. Please provide a percentage")
   end
 
   apr
@@ -54,17 +48,14 @@ def get_loan_duration
   loop do
     prompt("Enter your loan duration in years:")
     duration_years = gets.chomp
-    if valid_number?(duration_years) == false
-      prompt("Invalid loan duration. Please provide a positive number")
-    else
-      break
-    end
+    break if valid_number?(duration_years) == true
+    prompt("Invalid loan duration. Please provide a positive number")
   end
 
   duration_years
 end
 
-def payment_calculation(loan_amount, apr, duration_years)
+def calculate_payment(loan_amount, apr, duration_years)
   loan_amount_float = loan_amount.to_f
   apr_decimal = apr.to_f / 100
   monthly_interest_rate = apr_decimal / 12
@@ -83,13 +74,17 @@ loop do
   apr = get_apr
   duration_years = get_loan_duration
 
-  monthly_payment = payment_calculation(loan_amount, apr, duration_years)
+  monthly_payment = calculate_payment(loan_amount, apr, duration_years)
+  total_payment = monthly_payment * duration_years.to_f * 12
+
+  prompt("For a loan of £#{loan_amount}, APR of #{apr} " \
+    "and payback period of #{duration_years} years...")
   prompt("Your monthly payment will be £#{format('%.2f', monthly_payment)}")
+  prompt("In total you will pay £#{format('%.2f', total_payment)}")
 
   prompt("Do you want to try again? (Enter Y to confirm)")
   repeat = gets.chomp
-  if repeat != "Y"
-    prompt("Goodbye!")
-    break
-  end
+  break unless repeat.downcase == "y"
 end
+
+prompt("Goodbye!")
